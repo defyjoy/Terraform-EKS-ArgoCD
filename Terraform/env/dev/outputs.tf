1d6717698eb6f5d2f5d53a75120ba93a3c381695
joydeep roychowdhury <joydeepbeyondsky86@gmail.com>
2025-03-25 15:42:43 +0530

    Fix: terraform and role arns

--- Terraform/env/dev/outputs.tf
+++ Terraform/env/dev/outputs.tf
@@ -2,3 +2,9 @@ output "external_dns_route53_zone_arns" {
   description = "description"
   value       = module.base.external_dns_route53_zone_arns
 }
+
+output "external_dns_role_arn" {
+  description = "description"
+  value       = module.base.external_dns_role_arn
+}
+
